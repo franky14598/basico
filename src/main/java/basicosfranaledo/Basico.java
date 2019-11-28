9f1249eb119b7293fd778fb5eef8e9afe385a788
Fran Aledo <Fran.aledo@hotmail.com>
2019-11-28 10:01:54 +0100

    numericos

--- src/main/java/basicosfranaledo/Basico.java
+++ src/main/java/basicosfranaledo/Basico.java
@@ -11,6 +11,26 @@ package basicosfranaledo;
  */
 public class Basico {
 
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
     /**
      * @param args the command line arguments
      */
