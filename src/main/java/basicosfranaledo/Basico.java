f25e5decdd0dfc70a3c7d672f03404e015b1ca9c
Fran Aledo <Fran.aledo@hotmail.com>
2019-11-28 10:05:46 +0100

    cadenas

--- src/main/java/basicosfranaledo/Basico.java
+++ src/main/java/basicosfranaledo/Basico.java
@@ -11,10 +11,20 @@ package basicosfranaledo;
  */
 public class Basico {
 
+    public static void cadenas(){
+    
+        String hola="HOLA", adios="ADIOS";
+        
+        System.out.println(hola);
+        System.out.println(adios);
+    
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        cadenas();
         // TODO code application logic here
     }
     
